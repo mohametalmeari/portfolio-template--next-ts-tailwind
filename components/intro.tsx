45de3e3237a743eadb194932f497968506f22999
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-18 00:52:19 +0300

    Create Contact me button

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
 
 const Intro = () => {
   return (
@@ -42,9 +44,10 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
-      <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">{`Hello, I'm Mohamad. `}</span> {`I'm a `}
         <span className="font-bold">{`full-stack developer `}</span> {`with `}
@@ -53,6 +56,15 @@ const Intro = () => {
         <span className="italic">{`sites & apps `}</span> {`. My focus is `}
         <span className="underline">{`React (Next.js)`}</span>
       </motion.p>
+
+      <div>
+        <Link
+          href="#contact"
+          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full justify-center"
+        >
+          Contact me here <BsArrowRight />
+        </Link>
+      </div>
     </section>
   );
 };
