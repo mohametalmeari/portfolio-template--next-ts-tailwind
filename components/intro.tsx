8cdb1d1ab5f406d047a1e8ae68e907a2759d7427
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-18 00:45:16 +0300

    Animate intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -42,14 +42,17 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
-      <p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl ">
+      <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      >
         <span className="font-bold">{`Hello, I'm Mohamad. `}</span> {`I'm a `}
         <span className="font-bold">{`full-stack developer `}</span> {`with `}
         <span className="font-bold">{`2 years `}</span>
         {`of experience. I enjoy building `}
         <span className="italic">{`sites & apps `}</span> {`. My focus is `}
         <span className="underline">{`React (Next.js)`}</span>
-      </p>
+      </motion.p>
     </section>
   );
 };
