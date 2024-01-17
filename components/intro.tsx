6f5c4fa50f5b5805a3754920ccf3746161140f46
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-17 23:44:00 +0300

    Animate profile image

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,21 +1,45 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Intro = () => {
   return (
     <section>
       <div className="flex items-center justify-center">
         <div className="relative">
-          <Image
-            src="https://cdn-icons-png.flaticon.com/512/180/180658.png"
-            alt="Personal Portrait"
-            width="192"
-            height="192"
-            quality="95"
-            priority={true}
-            className="h-24 w-24 rounded-full object-cover bottom-[0.35rem] border-white shadow-lg"
-          />
-          <span className="absolute bottom-0 right-0 text-4xl">👋</span>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://cdn-icons-png.flaticon.com/512/180/180658.png"
+              alt="Personal Portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover bottom-[0.35rem] border-white shadow-lg"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
         </div>
       </div>
     </section>
