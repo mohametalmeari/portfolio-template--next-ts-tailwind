f7d5de54d01ba10ffb5fe30d8ae172364e5ea637
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-18 02:14:06 +0300

    Add href attribute to Downlaod CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -74,7 +74,11 @@ const Intro = () => {
           Contact me here
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full justify-center outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer">
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full justify-center outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
           Download CV
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
