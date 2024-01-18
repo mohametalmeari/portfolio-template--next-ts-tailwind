7dfdd08a60f65635074fa10d80e30f01e0270568
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-18 04:36:34 +0300

    Create projects.tsx

---
+++ components/projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionHeading from './section-heading';
+
+const Projects = () => {
+  return (
+    <section>
+      <SectionHeading>My projects</SectionHeading>
+    </section>
+  );
+};
+
+export default Projects;
