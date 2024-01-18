abe6962be6f20e4ae803874d39f727c7508a17c5
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-18 04:07:00 +0300

    Animate About section

--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <section className="mb-28 w-max-[45rem] text-center leading-8 sm:mb-40">
+    <motion.section
+      className="mb-28 w-max-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.175,
+      }}
+    >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         This paragraph should include your{" "}
@@ -13,7 +23,7 @@ const About = () => {
         This paragraph should include a <span className="italic">fun fact</span>{" "}
         about you.
       </p>
-    </section>
+    </motion.section>
   );
 };
 
