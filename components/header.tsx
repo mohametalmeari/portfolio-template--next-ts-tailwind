079d0135ba14ea862e99930b7d15a63b1719c0b9
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-18 22:19:30 +0300

    Let active section change on scrolling

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
@@ -8,7 +8,7 @@ import clsx from "clsx";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 const Header = () => {
-  const {activeSection, setActiveSection} = useActiveSectionContext()
+  const { activeSection, setActiveSection } = useActiveSectionContext();
 
   return (
     <header className="z-[999] relative">
