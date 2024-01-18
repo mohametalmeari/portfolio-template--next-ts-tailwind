584b1580eb51b7fad84cd9c262b7227af2238688
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-19 01:29:36 +0300

    Animate Skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,6 +4,21 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 const Skills = () => {
   const { ref } = useSectionInView("Skills");
@@ -15,12 +30,19 @@ const Skills = () => {
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li
+          <motion.li
             className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
             key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index} // Pass the index to the animation
           >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
