ce796e2db326855af3310c2249d28aa397301a72
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-19 15:41:09 +0300

    Replace alerts with toaster

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/send-email";
 import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 
 const Contact = () => {
   const { ref } = useSectionInView("Contact");
@@ -33,10 +34,10 @@ const Contact = () => {
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
           if (error) {
-            alert(error);
+            toast.error(error);
             return;
           }
-          alert("Email sent successfully!");
+          toast.success("Email sent!");
         }}
       >
         <input
