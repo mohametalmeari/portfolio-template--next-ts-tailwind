26675b79a8d1db71780fa94f91d28c02bd7e854e
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-19 19:02:43 +0300

    Import useEffect

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import {
   VerticalTimeline,
@@ -16,7 +16,7 @@ const Experience = () => {
   const { theme } = useTheme();
   const [isVisible, setIsVisible] = React.useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (inView) {
       setIsVisible(true);
     }
